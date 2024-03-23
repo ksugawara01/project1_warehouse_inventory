cf421459df7f32068bb346237fd49271027f0855
Koudou Sugawara <ksugawara01@gmail.com>
2024-03-23 16:36:34 -0400

    add the getAllFruits() method

--- backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/controllers/FruitController.java
+++ backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/controllers/FruitController.java
@@ -1,9 +1,15 @@
 package com.skillstorm.warehouseinventoryproject.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skillstorm.warehouseinventoryproject.models.Fruit;
 import com.skillstorm.warehouseinventoryproject.services.FruitService;
 
 @RestController
@@ -16,7 +22,12 @@ public class FruitController {
     // create fruit
 
     // view all fruits
-
+    @GetMapping
+    public ResponseEntity<List<Fruit>> getAllFruits() {
+        List<Fruit> fruits = fruitService.getAllFruits();
+        return new ResponseEntity<List<Fruit>>(fruits, HttpStatus.OK);
+    }
+    
     // view fruit by id
 
     // update fruit by id
--- backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/services/FruitService.java
+++ backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/services/FruitService.java
@@ -1,8 +1,22 @@
 package com.skillstorm.warehouseinventoryproject.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skillstorm.warehouseinventoryproject.models.Fruit;
+import com.skillstorm.warehouseinventoryproject.repositories.FruitRepository;
 
 @Service
 public class FruitService {
     
+    @Autowired
+    FruitRepository fruitRepository;
+
+    public List<Fruit> getAllFruits() {
+        List<Fruit> fruits = fruitRepository.findAll();
+        
+        return fruits;
+    }
 }
