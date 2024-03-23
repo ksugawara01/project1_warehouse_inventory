3ef1eb7a3fe375d91a53e46ac3a046bbfa01f5f4
Koudou Sugawara <ksugawara01@gmail.com>
2024-03-23 12:00:53 -0400

    initial commit

---
+++ backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/WarehouseInventoryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.warehouseinventoryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseInventoryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseInventoryProjectApplication.class, args);
+	}
+
+}
---
+++ backend/warehouse-inventory-project/src/test/java/com/skillstorm/warehouseinventoryproject/WarehouseInventoryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.warehouseinventoryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseInventoryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
