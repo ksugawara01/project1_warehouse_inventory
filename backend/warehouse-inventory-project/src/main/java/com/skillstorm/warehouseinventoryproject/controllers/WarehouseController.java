a7e5dbb3c391b8502859de4409205a2d1ce8c138
Koudou Sugawara <ksugawara01@gmail.com>
2024-03-24 17:20:30 -0400

    add warehouse crud methods

--- backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/controllers/WarehouseController.java
+++ backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/controllers/WarehouseController.java
@@ -1,9 +1,21 @@
 package com.skillstorm.warehouseinventoryproject.controllers;
 
+import java.util.List;
+
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skillstorm.warehouseinventoryproject.models.Warehouse;
 import com.skillstorm.warehouseinventoryproject.services.WarehouseService;
 
 @RestController
@@ -14,12 +26,42 @@ public class WarehouseController {
     WarehouseService warehouseService;
 
     // create warehouse
+    @PostMapping("/warehouse")
+    public ResponseEntity<Warehouse> createWarehouse(@RequestBody Warehouse warehouse) {
+        Warehouse newWarehouse = warehouseService.createWarehouse(warehouse);
+
+        return new ResponseEntity<Warehouse>(newWarehouse, HttpStatus.CREATED);
+    }
     
     // view all warehouses
+    @GetMapping
+    public ResponseEntity<List<Warehouse>> getAllWarehouses() {
+        List<Warehouse> warehouses = warehouseService.getAllWarehouses();
+
+        return new ResponseEntity<List<Warehouse>>(warehouses, HttpStatus.OK);
+    }
 
     // view warehouse by id
+    @GetMapping("/warehouse/{id}")
+    public ResponseEntity<Warehouse> getWarehouseById(@PathVariable int id) {
+        Warehouse warehouse = warehouseService.getWarehouseById(id);
+
+        return new ResponseEntity<Warehouse>(warehouse, HttpStatus.OK);
+    }
 
     // update warehouse by id
+    @PutMapping("/warehouse")
+    public ResponseEntity<Warehouse> updateWarehouse(@RequestBody Warehouse warehouse) {
+        Warehouse updatedWarehouse = warehouseService.updateWarehouse(warehouse);
+
+        return new ResponseEntity<Warehouse>(updatedWarehouse, HttpStatus.OK);
+    }
 
     // delete warehouse by id
+    @DeleteMapping("/warehouse")
+    public ResponseEntity<Integer> deleteWarehouse(@RequestBody Warehouse warehouse) {
+        warehouseService.deleteWarehouse(warehouse);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/services/WarehouseService.java
+++ backend/warehouse-inventory-project/src/main/java/com/skillstorm/warehouseinventoryproject/services/WarehouseService.java
@@ -1,8 +1,49 @@
 package com.skillstorm.warehouseinventoryproject.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skillstorm.warehouseinventoryproject.models.Warehouse;
+import com.skillstorm.warehouseinventoryproject.repositories.WarehouseRepository;
 
 @Service
 public class WarehouseService {
     
+    @Autowired
+    WarehouseRepository warehouseRepository;
+
+    public Warehouse createWarehouse(Warehouse warehouse) {
+        Warehouse newWarehouse = warehouseRepository.save(warehouse);
+
+        return newWarehouse;
+    }
+    
+    public List<Warehouse> getAllWarehouses() {
+        List<Warehouse> warehouses = warehouseRepository.findAll();
+
+        return warehouses;
+    }
+
+    public Warehouse getWarehouseById(int id) {
+        Optional<Warehouse> warehouse = warehouseRepository.findById(id);
+
+        if (warehouse.isPresent()) {
+            return warehouse.get();
+        }
+
+        return null;
+    }
+
+    public Warehouse updateWarehouse(Warehouse warehouse) {
+        Warehouse updatedWarehouse = warehouseRepository.save(warehouse);
+
+        return updatedWarehouse;
+    }
+
+    public void deleteWarehouse(Warehouse warehouse) {
+        warehouseRepository.delete(warehouse);
+    }
 }
